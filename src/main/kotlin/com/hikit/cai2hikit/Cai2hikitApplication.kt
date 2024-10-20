c10b62aff0598c9b1877db4d7eb9392e7fdc6f4c
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-10-20 17:00:06 +0200

    #1 spring init

---
+++ src/main/kotlin/com/hikit/cai2hikit/Cai2hikitApplication.kt
@@ -0,0 +1,11 @@
+package com.hikit.cai2hikit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Cai2hikitApplication
+
+fun main(args: Array<String>) {
+	runApplication<Cai2hikitApplication>(*args)
+}
