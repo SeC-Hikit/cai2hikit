cfa41a7b6ce7e32f115a9b97ecc79008684aedb8
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-10-23 13:20:21 +0200

    Changed API path to api/v1/

--- src/main/kotlin/com/hikit/cai2hikit/TrailController.kt
+++ src/main/kotlin/com/hikit/cai2hikit/TrailController.kt
@@ -1,17 +1,16 @@
 package com.hikit.cai2hikit
 
 import Trail
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.atomic.AtomicLong
 
+
 @RestController
+@RequestMapping("/api/v1/trail")
 class TrailController {
     private val counter = AtomicLong()
 
-    @GetMapping("/trail/{code}")
+    @GetMapping("/{code}")
     fun getTrail(@PathVariable code: Int): Trail {
         return Trail(counter.incrementAndGet(), code)
     }
