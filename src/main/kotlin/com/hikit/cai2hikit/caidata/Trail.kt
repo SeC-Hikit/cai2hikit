a51f952f48334db6065d55f11aef3334d53fce21
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-11-06 14:47:30 +0100

    #2 fixed date data and changed Trail to data class

--- src/main/kotlin/com/hikit/cai2hikit/caidata/Trail.kt
+++ src/main/kotlin/com/hikit/cai2hikit/caidata/Trail.kt
@@ -1,119 +1,23 @@
 package com.hikit.cai2hikit.caidata
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import org.springframework.data.annotation.Id
 import org.bson.types.ObjectId
 import java.util.*
 
-class Trail {
-    @Id private var id: ObjectId = ObjectId()
-
-    private var relation_id: Int = 0
-    private var source: String = ""
-    private var cai_scale: String = ""
-    private var from: String = ""
-    private var to: String = ""
-    private var ref: String = ""
-    private var public_page: String = ""
-    private var sda: Int = 0
-    private var validation_date: Date = Date()
-    private var updated_at: Date = Date()
-
-    private var coordinates: Array<Coordinates2D> = emptyArray()
-
-    fun getId(): String {
-        return id.toString()
-    }
-
-    fun setId(id: Int) {
-        val date = Date()
-        this.id = ObjectId(date, id)
-    }
-
-    fun getRelationId(): Int {
-        return relation_id
-    }
-
-    fun setRelationId(relation_id: Int) {
-        this.relation_id = relation_id
-    }
-
-    fun getSource(): String {
-        return source
-    }
-
-    fun setSource(source: String) {
-        this.source = source
-    }
-
-    fun getCai_scale(): String {
-        return cai_scale
-    }
-
-    fun setCai_scale(cai_scale: String) {
-        this.cai_scale = cai_scale
-    }
-
-    fun getFrom(): String {
-        return from
-    }
-
-    fun setFrom(from: String) {
-        this.from = from
-    }
-
-    fun getTo(): String {
-        return to
-    }
-
-    fun setTo(to: String) {
-        this.to = to
-    }
-
-    fun getRef(): String {
-        return ref
-    }
-
-    fun setRef(ref: String) {
-        this.ref = ref
-    }
-
-    fun getPublicPage(): String {
-        return public_page
-    }
-
-    fun setPublicPage(public_page: String) {
-        this.public_page = public_page
-    }
-
-    fun getSda(): Int {
-        return sda
-    }
-
-    fun setSda(sda: Int) {
-        this.sda = sda
-    }
-
-    fun getValidation_date(): Date {
-        return validation_date
-    }
-
-    fun setValidationDate(validation_date: Date) {
-        this.validation_date = validation_date
-    }
-
-    fun getUpdated_at(): Date {
-        return updated_at
-    }
-
-    fun setUpdatedAt(updated_at: Date) {
-        this.updated_at = updated_at
-    }
-
-    fun getCoordinates(): Array<Coordinates2D> {
-        return coordinates
-    }
-
-    fun setCoordinates(coordinates: Array<Coordinates2D>) {
-        this.coordinates = coordinates
-    }
-}
+data class Trail(
+    val id: String,
+    @Id val relation_id: Int,
+    val source: String,
+    val cai_scale: String,
+    val from: String,
+    val to: String,
+    val ref: String,
+    val public_page: String,
+    val sda: Int,
+    @JsonFormat(pattern = "Yyyy-mm-dd")
+    val validation_date: Date,
+    @JsonFormat (pattern = "Yyyy-mm-dd HH:mm:ss")
+    val updated_at: Date,
+    val coordinates: Array<Coordinates2D>
+)
