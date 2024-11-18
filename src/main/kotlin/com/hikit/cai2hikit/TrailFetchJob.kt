0f954028e1b97ee16ac720f0cb24c04d6c6b0a4f
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-11-18 13:03:23 +0100

    #6 hiking list download

--- src/main/kotlin/com/hikit/cai2hikit/TrailFetchJob.kt
+++ src/main/kotlin/com/hikit/cai2hikit/TrailFetchJob.kt
@@ -1,6 +1,7 @@
 package com.hikit.cai2hikit
 
 import com.hikit.cai2hikit.dto.Trail
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.MediaType
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -14,6 +15,14 @@ class TrailFetchJob {
     @Scheduled(cron = "\${job.fetch.chron}")
     fun getTrail() {
         // TODO: get all ER trails
+        // BB should be bigger
+        val trailsResponse = restClient.get()
+            .uri("hiking-routes/bb/44.810096,43.741833,11.355102,12.036322/4")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .body(object : ParameterizedTypeReference<Any?>() {})
+
+        println(trailsResponse)
 
         // TODO: persist all of them querying them one by one with a short interruption in the middle (~1s)
         val serializedResponse = restClient.get()
@@ -24,7 +33,6 @@ class TrailFetchJob {
 
         // TODO exclude non relevant trails
         println(serializedResponse)
-        // TODO
     }
 
 }
