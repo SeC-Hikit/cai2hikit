3c049aea333f657abc1504dc71e29406cf731c27
Lorenzo Verri <lore.verri@gmail.com>
2024-11-18 23:11:22 +0100

    feature(6): add mapping

--- src/main/kotlin/com/hikit/cai2hikit/TrailFetchJob.kt
+++ src/main/kotlin/com/hikit/cai2hikit/TrailFetchJob.kt
@@ -1,14 +1,15 @@
 package com.hikit.cai2hikit
 
-import com.fasterxml.jackson.core.type.TypeReference
+import com.hikit.cai2hikit.dto.IdToUpdateDate
 import com.hikit.cai2hikit.dto.Trail
-import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.MediaType
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
-import org.springframework.web.client.body
-import kotlin.reflect.typeOf
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+private const val dateTimeFormat = "yyyy-MM-dd HH:mm:ss"
 
 @Service
 class TrailFetchJob {
@@ -18,29 +19,33 @@ class TrailFetchJob {
     @Scheduled(cron = "\${job.fetch.chron}")
     fun getTrail() {
 
-        var hikes = LinkedHashMap<Int, String>()
-        // TODO: get all ER trails
-        // BB should be bigger
+
+        // TODO make the bounding box configurable
         val trailsResponse = restClient.get()
             .uri("hiking-routes/bb/44.810096,43.741833,11.355102,12.036322/4")
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
-            .body(String::class.java)
-//            .body(LinkedHashMap<Int, String>())
-//            .body(object : ParameterizedTypeReference<Any?>() {})
+            .body(Map::class.java)
+            ?.map {
+                // TODO: test this
+                IdToUpdateDate(
+                    it.key.toString(),
+                    LocalDateTime.parse(it.value.toString(), DateTimeFormatter.ofPattern(dateTimeFormat))
+                )
+            }
 
         // automatically casts to java.util.LinkedHashMap
         println(trailsResponse!!::class.java.typeName)
         println(trailsResponse)
 
+        // TODO: loop over all trails and get the data by hike id
+
         val hike_id = 11581
         // TODO: persist all of them querying them one by one with a short interruption in the middle (~1s)
-       val serializedResponse = restClient.get()
+        val serializedResponse = restClient.get()
             .uri("hiking-route/${hike_id}")
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
             .body(Trail::class.java)
-        // TODO exclude non relevant trails
-        println(serializedResponse)
     }
 }
