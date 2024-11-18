89df18f073b27a00f14b688411f7f491f8cfab8f
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-11-18 20:23:04 +0100

    #6 hiking list download pt 2

--- src/main/kotlin/com/hikit/cai2hikit/TrailFetchJob.kt
+++ src/main/kotlin/com/hikit/cai2hikit/TrailFetchJob.kt
@@ -1,11 +1,14 @@
 package com.hikit.cai2hikit
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.hikit.cai2hikit.dto.Trail
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.MediaType
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
+import kotlin.reflect.typeOf
 
 @Service
 class TrailFetchJob {
@@ -14,25 +17,30 @@ class TrailFetchJob {
 
     @Scheduled(cron = "\${job.fetch.chron}")
     fun getTrail() {
+
+        var hikes = LinkedHashMap<Int, String>()
         // TODO: get all ER trails
         // BB should be bigger
         val trailsResponse = restClient.get()
             .uri("hiking-routes/bb/44.810096,43.741833,11.355102,12.036322/4")
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
-            .body(object : ParameterizedTypeReference<Any?>() {})
+            .body(String::class.java)
+//            .body(LinkedHashMap<Int, String>())
+//            .body(object : ParameterizedTypeReference<Any?>() {})
 
+        // automatically casts to java.util.LinkedHashMap
+        println(trailsResponse!!::class.java.typeName)
         println(trailsResponse)
 
+        val hike_id = 11581
         // TODO: persist all of them querying them one by one with a short interruption in the middle (~1s)
-        val serializedResponse = restClient.get()
-            .uri("hiking-route/11581")
+       val serializedResponse = restClient.get()
+            .uri("hiking-route/${hike_id}")
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
             .body(Trail::class.java)
-
         // TODO exclude non relevant trails
         println(serializedResponse)
     }
-
 }
