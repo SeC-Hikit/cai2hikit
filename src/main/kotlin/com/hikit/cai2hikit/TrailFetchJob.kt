26289df335cc4c59baacb3baa2b648958966b13d
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-11-20 00:34:02 +0100

    #6 hiking list download and db save

--- src/main/kotlin/com/hikit/cai2hikit/TrailFetchJob.kt
+++ src/main/kotlin/com/hikit/cai2hikit/TrailFetchJob.kt
@@ -1,7 +1,11 @@
 package com.hikit.cai2hikit
 
+import org.springframework.data.mongodb.repository.MongoRepository
+
 import com.hikit.cai2hikit.dto.IdToUpdateDate
 import com.hikit.cai2hikit.dto.Trail
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -12,40 +16,52 @@ import java.time.format.DateTimeFormatter
 private const val dateTimeFormat = "yyyy-MM-dd HH:mm:ss"
 
 @Service
-class TrailFetchJob {
+class TrailFetchJob(
+        @Value("\${job.fetch.bblatmin}") val bblatmin: String,
+        @Value("\${job.fetch.bblatmax}") val bblatmax: String,
+        @Value("\${job.fetch.bblongmin}") val bblongmin: String,
+        @Value("\${job.fetch.bblongmax}") val bblongmax: String
+    ) {
+
+    @Autowired
+    private lateinit var trailRepository: TrailRepository
 
     private val restClient: RestClient = RestClient.builder().baseUrl("https://osm2cai.it/api/v2/").build()
 
     @Scheduled(cron = "\${job.fetch.chron}")
     fun getTrail() {
-
+        println(trailRepository.findAll())
+        val uriString = "hiking-routes/bb/$bblatmax,$bblatmin,$bblongmin,$bblongmax/4"
+        println(uriString)
 
         // TODO make the bounding box configurable
         val trailsResponse = restClient.get()
-            .uri("hiking-routes/bb/44.810096,43.741833,11.355102,12.036322/4")
+            .uri(uriString)
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
             .body(Map::class.java)
             ?.map {
-                // TODO: test this
                 IdToUpdateDate(
                     it.key.toString(),
                     LocalDateTime.parse(it.value.toString(), DateTimeFormatter.ofPattern(dateTimeFormat))
                 )
             }
 
-        // automatically casts to java.util.LinkedHashMap
-        println(trailsResponse!!::class.java.typeName)
-        println(trailsResponse)
+        //TODO : store in DB
+        if (trailsResponse != null) {
+            for(trailId in trailsResponse) {
+                val serializedResponse = restClient.get()
+                    .uri("hiking-route/${trailId.id}")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .body(Trail::class.java)
 
-        // TODO: loop over all trails and get the data by hike id
-
-        val hike_id = 11581
-        // TODO: persist all of them querying them one by one with a short interruption in the middle (~1s)
-        val serializedResponse = restClient.get()
-            .uri("hiking-route/${hike_id}")
-            .accept(MediaType.APPLICATION_JSON)
-            .retrieve()
-            .body(Trail::class.java)
+                println(serializedResponse)
+                if (serializedResponse != null) {
+                    trailRepository.save(Trail(serializedResponse.properties, serializedResponse.geometry))
+                }
+                Thread.sleep(1_000)
+            }
+        }
     }
 }
