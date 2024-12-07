4b6f46c90d65aebcd286da96ad00e42d4c81604a
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-12-07 12:10:02 +0100

    trying to build a test with bean injection

--- src/main/kotlin/com/hikit/cai2hikit/TrailRestClient.kt
+++ src/main/kotlin/com/hikit/cai2hikit/TrailRestClient.kt
@@ -1,8 +1,5 @@
 package com.hikit.cai2hikit
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
 
 class TrailRestClient(baseUrl: String) {
--- src/test/kotlin/com/hikit/cai2hikit/TrailFetchJobTest.kt
+++ src/test/kotlin/com/hikit/cai2hikit/TrailFetchJobTest.kt
@@ -1,16 +1,26 @@
 package com.hikit.cai2hikit
 
+import com.hikit.cai2hikit.dto.Trail
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.springframework.http.MediaType
 
-class TrailFetchJobTest {
+
+class TrailFetchJobTest(
+    val mockedTrailClient: TrailRestClient
+) {
+//    var mockedTrailClient: TrailRestClient = mock(TrailRestClient::class.java)
 
     @Test
     fun `should test retrieving one trail member calls`() {
-        // given
-
-        // when
-
-        // then
+        //  should I use \${test.fetch.trailid} and application.yml?
+        val singleTrailFetch = "http://osm2cai.it/api/v2/hiking-route/30319"
+        val trailTestResponse = mockedTrailClient.getRestClient().get()
+            .uri(singleTrailFetch)
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .body(Trail::class.java)
+        print(trailTestResponse)
     }
 
 }
