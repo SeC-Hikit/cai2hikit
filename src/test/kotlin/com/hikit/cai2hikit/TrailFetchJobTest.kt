6f028751a07af5a362134e0cfcea72f0ff007895
Lorenzo Verri <lore.verri@gmail.com>
2024-12-09 16:14:37 +0100

    feature(6): add extension

--- src/test/kotlin/com/hikit/cai2hikit/TrailFetchJobTest.kt
+++ src/test/kotlin/com/hikit/cai2hikit/TrailFetchJobTest.kt
@@ -2,12 +2,14 @@ package com.hikit.cai2hikit
 
 import com.hikit.cai2hikit.dto.IdToUpdateDate
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
 import java.time.LocalDateTime
 
-
+@ExtendWith(MockitoExtension::class)
 class TrailFetchJobTest(
     @Mock val mockedTrailClient: TrailRestClient
 ) {
