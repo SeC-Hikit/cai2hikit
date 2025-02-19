bb64b8452c295d767dbffdda240dcf2f89847932
ARtbibaOfficial <giovanni.basso@icloud.com>
2025-02-19 14:02:39 +0100

    adding altitude wrapper

---
+++ src/main/kotlin/com/hikit/cai2hikit/adapter/AltitudeServiceWrapper.kt
@@ -0,0 +1,23 @@
+package com.hikit.cai2hikit.adapter
+
+import com.hikit.cai2hikit.dao.Coordinates
+import com.hikit.cai2hikit.dto.Coordinates2D
+import org.sc.common.rest.CoordinatesDto
+import org.hikit.common.adapter.AltitudeServiceAdapter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class AltitudeServiceWrapper @Autowired constructor(
+    private val altitudeServiceAdapter: AltitudeServiceAdapter
+) {
+    fun mapCoordsWithElevations(coordinates: List<Coordinates2D>): List<Coordinates> =
+        altitudeServiceAdapter.getElevationsByLongLat(coordinates.map { Pair(it.latitude, it.longitude) })
+            .mapIndexed{ index, altitude ->
+                CoordinatesDto(coordinates[index].latitude,
+                    coordinates[index].longitude, altitude)
+            }
+
+    fun getElevationsByLongLat(latitude: Double, longitude: Double) = altitudeServiceAdapter.getElevationsByLongLat(latitude, longitude)
+    fun getElevationsByLongLat(coordinates: List<Pair<Double, Double>>) = altitudeServiceAdapter.getElevationsByLongLat(coordinates)
+}
