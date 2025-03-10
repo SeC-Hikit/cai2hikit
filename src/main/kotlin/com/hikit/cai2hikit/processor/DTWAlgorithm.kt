0797d5820dd9a9d34dca8f6dc5ea7013e7c3a89a
ARtbibaOfficial <giovanni.basso@icloud.com>
2025-03-10 11:27:56 +0100

    fixed dtwalgorithm

--- src/main/kotlin/com/hikit/cai2hikit/processor/DTWAlgorithm.kt
+++ src/main/kotlin/com/hikit/cai2hikit/processor/DTWAlgorithm.kt
@@ -13,25 +13,24 @@ import kotlin.math.sqrt
 class DTWAlgorithm @Autowired constructor(
     val trailRepository: TrailRepository
 ) {
-    private fun coordinatesDistance(s: List<Double>, t: List<Double>): Double {
-        val tempLat = (s[0] - t[0]).pow(2)
-        val tempLon = (s[1] - t[1]).pow(2)
-        return sqrt(tempLat + tempLon)
-    }
-
-    fun runAlgorithm(trailId: String, trailIn_1: Trail): Double {
-        val trailIn_0 = trailRepository.findByPropsId(trailId)
-        val DTW = Array(trailIn_0!!.geometry.coordinates[0].size) {Array(trailIn_1.geometry.coordinates[0].size) {99.0}}
+    fun runAlgorithm(coords1: List<List<Double>>, coords2: List<List<Double>>): Double {
+        val DTW = Array(coords1.size) {Array(coords2.size) {99.0}}
         DTW[0][0] = 0.0
 
         for(i in 1 until DTW.size) {
             for(j in 1 until DTW[i].size) {
-                val cost = coordinatesDistance(trailIn_0.geometry.coordinates[0][i], trailIn_1.geometry.coordinates[0][j])
+                val cost = coordinatesDistance(coords1[i], coords2[j])
                 DTW[i][j] = cost + min(min(DTW[i-1][j], DTW[i][j-1]), DTW[i-1][j-1])
             }
         }
         val result = 1.0 - min(DTW[DTW.size - 1][DTW[DTW.size - 1].size - 1], 1.0)
 
         return(result)
+    }
+
+    private fun coordinatesDistance(s: List<Double>, t: List<Double>): Double {
+        val tempLat = (s[0] - t[0]).pow(2)
+        val tempLon = (s[1] - t[1]).pow(2)
+        return sqrt(tempLat + tempLon)
     }
 }
