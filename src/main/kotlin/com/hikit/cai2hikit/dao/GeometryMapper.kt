9f5fa478d075b7abf144d473c256362bf68cd859
ARtbibaOfficial <giovanni.basso@icloud.com>
2025-03-17 17:07:55 +0100

    trailsimilarity fix

--- src/main/kotlin/com/hikit/cai2hikit/dao/GeometryMapper.kt
+++ src/main/kotlin/com/hikit/cai2hikit/dao/GeometryMapper.kt
@@ -5,10 +5,6 @@ import org.springframework.stereotype.Component
 
 @Component
 class GeometryMapper {
-    fun listToCoords2D(toMap: List<Double>): List<Coordinates2D> {
-        return
-    }
-
     fun dtoToCoords2D(toMap: org.hikit.common.dto.Geometry): List<Coordinates2D> {
         return toMap.coordinates.map {
             Coordinates2D(it[0], it[1])
